18513b12804b2d8738f675a3159af24c4ed76e61
gottben <gottbenn@bu.edu>
2017-04-13 20:56:44 -0400

    Updates with more models

--- LRegress.R
+++ LRegress.R
@@ -12,14 +12,14 @@ transformData <- log10(myData)
 
 # You may also add interaction terms of the form nifB:nifH, nifN:nifE:nifM, etc. to the equation
 
-fit <- lm(yield ~ nifB + nifN + nifE + nifM + nifU + nifH + nifD + nifS + nifK, data=transformData)
+fit <- lm(yield ~ nifB + nifN + nifE + nifM + nifS + nifK, data=transformData)
 
 # Displays summary of regression model, including coefficients, p-values, R^2, etc.
 
 summary(fit)
 
 # Creates residual plots for regression model
-
+dev.off()
 layout(matrix(c(1,2,3,4),2,2))
 
 plot(fit)
@@ -28,12 +28,13 @@ plot(fit)
 
 library(leaps) 
 
-leaps <- regsubsets(yield ~ (nifB + nifN + nifE + nifM + nifU + nifH + nifD + nifS + nifK)^2, data=transformData, nbest=6, nvmax=11, force.in=c(1,2,3,4,5,6))
+leaps <- regsubsets(yield ~ (nifB + nifN + nifE + nifM + nifS + nifK)^2, data=transformData, nbest=6, nvmax=11, force.in=c(1,2,3,4,5,6))
 
 plot(leaps, scale="adjr2")
 
 # Uses regression model to predict yield for a new set of gene expression levels
 
-newData <- data.frame(nifB = log10(5000), nifN = log10(6000), nifE = log10(7000), nifM = log10(3000), nifU = log10(5000), nifH = log10(4000), nifD = log10(5000), nifS = log10(6000), nifK = log10(7000))
+newData <- data.frame(nifB = log10(67),  nifE = log10(500), nifK = log10(14000), nifM = log10(10000), nifN = log10(10000), nifS = log10(12000))
 
 predictedYield <- 10^predict(fit, newData)
+
